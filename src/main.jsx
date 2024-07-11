122fbcd27f02bb5a25afe5d7e754744687e9fcee
VISHAL CHAURASIYA <121449551+vishal7908@users.noreply.github.com>
2024-07-11 18:33:09 +0530

    first commit

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import { Provider } from "react-redux";
 import store from "./app/store.js";
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </Provider>
